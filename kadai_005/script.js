1e83c4e4a7864a643aa7bee181b269b6a9e9b4c7
seishu338 <seikuma338@icloud.com>
2023-12-03 09:00:06 +0900

    課題5の追加

---
+++ kadai_005/script.js
@@ -0,0 +1,29 @@
+$(function(){
+    $('.button-more').on('mouseover', function(){
+       $(this).animate({
+        opacity:0.5,
+        marginLeft:20,
+       }, 100); 
+    });
+
+    $('.button-more').on('mouseout', function(){
+        $(this).animate({
+            opacity:1,
+            marginLeft: 0
+        }, 100);
+    });
+
+    $('.carousel').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+      });
+
+      $('.carousel').on('afterChange', function(event, slick, currentSlide){
+        // スライダーのスライドが切り替わった後、再度再生を開始
+        $('.carousel').slick('slickPlay');
+    });
+    
+});
